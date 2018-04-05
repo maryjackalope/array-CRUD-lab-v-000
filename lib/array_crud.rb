61961d234cdd8b2a8b83de11acbb275b85a0bc84
Mary Jack <maryjackinthebox@gmail.com>
2018-04-05 00:48:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- c= [1,2,3,4]
-  c.push("arrays!")
+ array= [1,2,3,4]
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
