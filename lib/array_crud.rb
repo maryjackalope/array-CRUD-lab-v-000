e6e21c6c246510512bb58c0f894d9c2375561cb9
Mary Jack <maryjackinthebox@gmail.com>
2018-04-05 00:50:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,7 @@ def add_element_to_start_of_array(array, element)
   array.unshift("wow") 
 end
 
-def remove_element_from_end_of_array(array, element)
+def remove_element_from_end_of_array(array)
    array = ["I", "am", "really", "learning", "arrays!"]
    array.pop("arrays!")
 end
